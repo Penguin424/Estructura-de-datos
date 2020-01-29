1fde94660c5bed17888b614ea4d7615cbd172251
Pablo Rizo <prozasoconw4@gmail.com>
2020-01-28 19:26:57 -0600

    Area y perimetro clase 28-01-2020

--- clase-28-01-2020.py
+++ clase-28-01-2020.py
@@ -10,6 +10,12 @@ def multi(n1,n2):
 def div(n1,n2):
     return n1 / n2
 
+def area(lado):
+    return lado**2
+
+def perimetro(lado):
+    return lado*4
+
 num1 = 5
 num2 = 8
 
@@ -22,3 +28,12 @@ print(res0)
 print(res1)
 print(res2)
 print(res3)
+
+print("\n")
+
+lado = int(input("Ingresa el lado del cuadrado: "))
+
+area_r = area(lado)
+perimetro_r = perimetro(lado)
+
+print("\nEl area del cuadrado es {} y el perimetro es {}".format(area_r, perimetro_r))
