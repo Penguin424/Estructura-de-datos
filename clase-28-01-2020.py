7a352614ac7e957edfb0e14f3300e3ef465d914e
Pablo Rizo <prozasoconw4@gmail.com>
2020-01-28 18:50:09 -0600

    Commit de clase del 28-01-2020

---
+++ clase-28-01-2020.py
@@ -0,0 +1,24 @@
+def suma(n1,n2):
+    return n1 + n2
+
+def rest(n1,n2):
+    return n1 - n2
+
+def multi(n1,n2):
+    return n1 * n2
+
+def div(n1,n2):
+    return n1 / n2
+
+num1 = 5
+num2 = 8
+
+res0 = suma(num1, num2)
+res1 = rest(num1, num2)
+res2 = multi(num1, num2)
+res3 = div(num1, num2)
+
+print(res0)
+print(res1)
+print(res2)
+print(res3)
